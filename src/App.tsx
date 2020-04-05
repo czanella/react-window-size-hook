dd1edbcf529752835403affa8229a010bef1f656
Carlos Zanella <cefzanella@gmail.com>
2020-04-04 21:18:36 -0300

    Initial commit - clean up CRA template, add useWindowSize

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useWindowSize } from './hooks';
+
+import styles from './App.module.scss';
 
 function App() {
+  const [width, height] = useWindowSize();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      [{`${width},${height}`}]
     </div>
   );
 }
---
+++ src/hooks/useWindowSize.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+export function useWindowSize(): Array<Number> {
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    const onResize = (): void => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', onResize);
+
+    onResize();
+
+    return () => window.removeEventListener('resize', onResize);
+  }, []);
+
+  return [width, height];
+}
