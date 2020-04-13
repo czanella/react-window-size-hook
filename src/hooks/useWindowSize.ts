1aeb720f8b0e95ac52ffb108f2bddbd2d54d009d
Carlos Zanella <cefzanella@gmail.com>
2020-04-12 22:40:22 -0300

    useWindowSize now returns a tuple instead of an array

--- src/hooks/useWindowSize.ts
+++ src/hooks/useWindowSize.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export function useWindowSize(): Array<Number> {
+export function useWindowSize(): [Number, Number] {
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
 
